c956bb6d1dd13eacb5f2d78e7ea96bc0653e769d
seanstern <sean.stern@outlook.com>
2022-11-16 12:24:13 -0500

    Update main/code/cache-client
    Formatting

--- main/code/cache-client.js
+++ main/code/cache-client.js
@@ -1,5 +1,11 @@
 import Cache from "./cache-v1.mjs";
 
 const cache = new Cache("file");
-cache.on("connected", () => console.log("cache connected"));
-cache.on("error", (err) => console.log(err));
+cache.on(
+  "connected",
+  () => console.log("cache connected")
+);
+cache.on(
+  "error",
+  (err) => console.log(err)
+);
